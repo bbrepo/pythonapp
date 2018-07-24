c84a070d77c160d54ea2803834048abcd73d422b
Bidhan Biswas <bidhan_009@yahoo.com>
2018-07-24 21:15:06 +0600

    Conditionals-Part 1

---
+++ pythonapp/conditionals.py
@@ -0,0 +1,22 @@
+#CONDITIONALS
+
+x=4
+
+#Basic If
+if x < 6:
+    print('This is true')
+
+#If Else
+if x < 6:
+    print('this is true')
+else:
+    print('This is false')
+    
+#Elif
+color='yellow'
+if color =='red':
+    print('Color is red')
+elif color =='blue':
+    print('Color is blue')
+else:
+    print('Color is not red or blue' )    
