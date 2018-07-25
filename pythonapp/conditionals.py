6cbc2a12a892fae494b1bf78884bd1f0e85444a6
Bidhan Biswas <bidhan_009@yahoo.com>
2018-07-25 21:34:36 +0600

    Conditionals

--- pythonapp/conditionals.py
+++ pythonapp/conditionals.py
@@ -1,22 +1,31 @@
-#CONDITIONALS
+# CONDITIONALS
 
 x=4
 
-#Basic If
+# Basic If
 if x < 6:
     print('This is true')
 
-#If Else
+# If Else
 if x < 6:
     print('this is true')
 else:
     print('This is false')
     
-#Elif
-color='yellow'
+# Elif
+color='red'
 if color =='red':
     print('Color is red')
 elif color =='blue':
     print('Color is blue')
 else:
-    print('Color is not red or blue' )    
+    print('Color is not red or blue' )   
+
+# Nested if
+if color=='red':
+    if x < 10:
+            print('Color is red and x is less than 10')
+            
+#Logical operators
+if color=='red' and x<10:
+    print('Color is red and x is less than 10')
